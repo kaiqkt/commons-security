e6e1757fda02e068e24a3f44e56f41fc4f47b18c
kaiqkt <kaiquebeserra1313@gmail.com>
2023-02-03 20:15:54 -0300

    fix: fixed value for ROLE_USER

--- src/main/kotlin/com/kaiqkt/commons/security/auth/PreAuthorizeRoles.kt
+++ src/main/kotlin/com/kaiqkt/commons/security/auth/PreAuthorizeRoles.kt
@@ -1,6 +1,6 @@
 package com.kaiqkt.commons.security.auth
 
-const val ROLE_USER = "ROLE_BUYER"
+const val ROLE_USER = "ROLE_USER"
 const val ROLE_SERVICE = "ROLE_SERVICE"
 
 const val AUTHORIZE_USER = "hasRole('$ROLE_USER')"
